3afab2cab9df4c60af59ef5937537b88acf25dcb
BahaaMekkawy <Bahaa.elmekkawy@gmail.com>
2025-09-02 03:26:06 +0300

    add-genericrepo-and-unitofwork

---
+++ src/Infrastructure/OnionEcommerceAPI.Infrastructure.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using OnionEcommerceAPI.Core.Domain.Common;
+using OnionEcommerceAPI.Core.Domain.Contracts;
+using OnionEcommerceAPI.Infrastructure.Persistence.Data;
+
+namespace OnionEcommerceAPI.Infrastructure.Persistence.Repositories
+{
+    internal class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        private readonly StoreContext _context;
+
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
+        {
+            if (withTracking)
+                return await _context.Set<TEntity>().ToListAsync();
+            return await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+        public async Task<TEntity?> GetAsync(TKey id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
---
+++ src/Infrastructure/OnionEcommerceAPI.Infrastructure.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+using OnionEcommerceAPI.Core.Domain.Common;
+using OnionEcommerceAPI.Core.Domain.Contracts;
+using OnionEcommerceAPI.Infrastructure.Persistence.Data;
+using OnionEcommerceAPI.Infrastructure.Persistence.Repositories;
+
+namespace OnionEcommerceAPI.Infrastructure.Persistence.UnitOfWork
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _context;
+        private readonly ConcurrentDictionary<string, object> repositories;
+
+        public UnitOfWork(StoreContext context)
+        {
+            _context = context;
+            repositories = new ConcurrentDictionary<string, object>();
+        }
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+            where TEntity : BaseEntity<TKey>
+            where TKey : IEquatable<TKey>
+        {
+            var repoName = typeof(TEntity).Name;
+            return (IGenericRepository<TEntity, TKey>)repositories.GetOrAdd(repoName, new GenericRepository<TEntity, TKey>(_context));
+        }
+
+        public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+        public ValueTask DisposeAsync() => _context.DisposeAsync();
+
+    }
+}
